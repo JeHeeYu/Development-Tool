403b6ed98779ab8fe872bf46f915b2c258452c3e
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2021-12-09 16:05:14 +0900

    mainwindow.h

---
+++ Qt/Example/Date,DateEdit/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDate>
+#include <QDateEdit>
+#include <QLabel>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+    QDateEdit *dateEdit[4];
+    QLabel *lb[6];
+
+private slots:
+    void clickFunc();
+
+};
+
+#endif // MAINWINDOW_H
