6f23071ce002d389e1f1306860a02a658874a2f5
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2021-12-13 13:19:53 +0900

    Update QByteArray.cpp

--- Qt/Class/QByteArray.cpp
+++ Qt/Class/QByteArray.cpp
@@ -7,8 +7,6 @@ QT += core
 
 /*      Method     */
 
-/* =============================== */
-
 QByteArray.append() : 배열의 뒤에 데이터를 이어 붙이기 위한 함수
 
 // Overloading
@@ -18,7 +16,7 @@ append(int count, char ch);
 append(const char *str);
 append(const char *str, int len);
 
-// Argument
+// Arguments
 ch : 추가할 문자
 *str : 추가할 문자열
 len : 문자열의 길이로 자동 결정되며 str보다 길면 안됨
@@ -42,7 +40,7 @@ prepend(int count, char ch);
 prepend(const char *str);
 prepend(const char *str, int len);
 
-// Argument
+// Arguments
 ch : 추가할 문자
 *str : 추가할 문자열
 len : 문자열의 길이로 자동 결정되며 str보다 길면 안됨
@@ -73,15 +71,15 @@ replace(const QByteArray &before, const char *after);
 replac(const char *before, const QByteArray &after);
 replace(char before, char after);
 
-// Argument
+// Arguments
 pos : 시작 위치로 배열 인덱스 번호
 len : pos위치에서부터의 길이
-&after : 뒤의 문자열을 변경할 문자열
+&after : 뒤의 문자열을 변경할 문자열로, QByteArray 객체
 *apfter : 뒤의 문자열을 변경할 문자열로, NULL로 끝나는 문자열로 변경할 수 있음
 asize : *after의 사이즈
 alen : pos와 같은 역할
 before : 지정한 부분의 앞의 문자
-&before : 앞의 문자열을 변경할 문자열
+&before : 앞의 문자열을 변경할 문자열로, QByteArray 객체
 *before : 앞의 문자열을 변경할 문자열로, NULL로 끝나는 문자열로 변경할 수 있음
 bsize : *before의 사이즈
 
@@ -91,3 +89,42 @@ QByteArray y("no");
 x.replace(4, 3, y);
 // x == "Say no!"
 
+/* =============================== */
+
+QByteArray.insert() : 원하는 위치에 문자 또는 문자열을 삽입하는 함수
+
+// Overloading
+insert(int i, const QByteArray &ba);
+insert(int i, char ch);
+insert(int i, int count, char ch);
+insert(int i, const char *str);
+insert(int i, const char *str, int len);
+
+// Arguments
+i : 삽입할 인덱스 위치
+&ba : 삽입할 문자 또는 문자열로 QByteArray 객체
+*str : 삽입할 문자열
+ch : 삽입할 문자
+len : 삽입할 문자열의 길이를 조정 (str의 len 바이트 만큼 삽입)
+count : 복사할 ch 갯수 (i 위치에 문자 ch의 복사본 삽입)
+
+// Example
+QByteArray ba("Meal");
+ba.insert(1, QByteArray("ontr"));
+// ba == "Montreal"
+
+/* =============================== */
+
+QByteArray.remove() : 원하는 위치에 문자열을 삭제하는 함수
+
+// Overloading
+remove(int pos, int len);
+
+// Arguments
+pos : 삭제할 인덱스 위치
+len : 삭제할 인덱스 크기
+
+// Example
+QByteArray ba("Montreal");
+ba.remove(1, 4);
+// ba == "Meal"
