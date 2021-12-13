0f02bce85a00da9978612e59a0a1a3538185704f
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2021-12-13 14:02:50 +0900

    Create QStringList.cpp

---
+++ Qt/Class/QStringList.cpp
@@ -0,0 +1,24 @@
+/*      Description     */
+문자열을 배열 형태로 관리할 수 있는 기능을 제공하는 클래스
+
+/*      Header, qmake     */
+#include <QStringList>
+QT += core
+
+/*      Constructor     */
+QStringList(InputIterator first, InputIterator last);
+QStringList(std::initializer_list<QString> args);
+QStringList(QList<QString> &other);
+QStringList(const QString &str);
+QStringList();
+
+/*      Method     */
+
+// Overloading
+
+// Arguments
+
+// Example
+
+
+/* =============================== */
