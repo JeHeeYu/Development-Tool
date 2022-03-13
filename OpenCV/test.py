9fcefd8cc2ba230144d39c9b313959b422f7faae
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-03-13 22:07:35 +0900

    Update test.py

--- OpenCV/test.py
+++ OpenCV/test.py
@@ -1,5 +1,5 @@
 import cv2 as cv
-from matplotlib.pyplot import gray
+import sys
 import numpy as np
 import math
 
@@ -14,39 +14,55 @@ def camera_on(dst, src):
     return cv.imshow(dst, src)
 
 def canny_edge_detection(image):
-    # 입력 영상을 그레이스케일 영상으로 변환
     input_image = grayscale_converter(image)
-    
-    # 그레이스케일로 변환된 영상에 가우시안 필터링 추가
     image_blur = cv.GaussianBlur(input_image, (5, 5), 0)
-    
-    # cv.Canny 함수는 캐니 에지 검출을 적용할 수 있는 함수
-    # cv.Canny 함수는 넓은 범위의 에지를 검출하는데 널리 사용되는 함수
     canny_image = cv.Canny(image_blur, 50, 150)
     return canny_image
 
-cap = camera_read()
+def roi_detection(image):
+    image_mask = np.zeros_like(image)
+    image_height = image.shape[0]
+    image_width = image.shape[1]
 
+    points = np.array([[(50, image_height), (290, 150), (400, 150), (image_width-100, image_height)]])
+    cv.fillPoly(image_mask, points, 255)
+    
+    line_image = cv.bitwise_and(image, image_mask)
+    return line_image
 
-while(True):
-    ret, cam = cap.read()
-    canny_cam = canny_edge_detection(cam)
+def draw_line(image, lines, color=[255, 0, 0], thickness=10):
+        for i in range(0, len(lines)):
+            l = lines[i][0]
+            cv.line(image, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv.LINE_AA)
+
+def hough_line(image, rho, theta, threshold, min_line_length, max_line_gap):
+    lines = cv.HoughLinesP(image, rho, theta, threshold, minLineLength = min_line_length, maxLineGap = max_line_gap)
+    cvt = cv.cvtColor(image, cv.COLOR_GRAY2BGR)
+    result_image = np.copy(cvt)
+    if lines is not None:   
+        draw_line(result_image, lines)
     
-    result_cam = cv.HoughLinesP(canny_cam, 1, np.pi/180, 50, None, 50, 10)
-    
-    if result_cam is not None:
-        for i in range(0, len(result_cam)):
-            l = result_cam[i][0]
-            cv.line(canny_cam, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv.LINE_AA)
-    
-    if(ret):
-        # GrayScale 변환 전 Camera On
-        #camera_on('camera', cam)
-        camera_on('graycam', canny_cam)
-        camera_on('houtghcam', result_cam)
+    return result_image
+
+def drive_start():
+    cap = camera_read()
+
+    while(True):
+        ret, cam = cap.read()
+        canny_cam = canny_edge_detection(cam)
+        roi = roi_detection(canny_cam)
         
-        if cv.waitKey(1) & 0xFF == 27:
-            break
+        hough = hough_line(roi, 1, np.pi / 180, 50, 50, 10)
+        result_cam = cv.addWeighted(cam, 0.001, hough, 0.8, 0.1, dtype=cv.CV_32F)
         
-#cap.released()
-cv.destroyAllWindows()
+        if(ret):
+            camera_on('Canny Camera', hough)
+            camera_on('Result Camera', result_cam)
+            
+            if cv.waitKey(1) & 0xFF == 27:
+                break
+        
+    cap.released()
+    cv.destroyAllWindows()
+            
+drive_start()
